203e102dd77a901440952b46c140d58f2c4df7bc
Ashwin Nanjappa <ashwin.n@outlook.com>
2014-01-23 10:58:52 +0800

    cutil.h added. Files fixed to compile with CUDA 5.5 and Ubuntu 12.04.

---
+++ source/fastHOG/HOG/cutil.h
@@ -0,0 +1,126 @@
+#pragma once
+
+// Give a little more for Windows : the console window often disapears before we can read the message
+#ifdef _WIN32
+# if 1//ndef UNICODE
+#  ifdef _DEBUG // Do this only in debug mode...
+	inline void VSPrintf(FILE *file, LPCSTR fmt, ...)
+	{
+		size_t fmt2_sz	= 2048;
+		char *fmt2		= (char*)malloc(fmt2_sz);
+		va_list  vlist;
+		va_start(vlist, fmt);
+		while((_vsnprintf(fmt2, fmt2_sz, fmt, vlist)) < 0) // means there wasn't anough room
+		{
+			fmt2_sz *= 2;
+			if(fmt2) free(fmt2);
+			fmt2 = (char*)malloc(fmt2_sz);
+		}
+		OutputDebugStringA(fmt2);
+		fprintf(file, fmt2);
+		free(fmt2);
+	}
+#	define FPRINTF(a) VSPrintf a
+#  else //debug
+#	define FPRINTF(a) fprintf a
+// For other than Win32
+#  endif //debug
+# else //unicode
+// Unicode case... let's give-up for now and keep basic printf
+#	define FPRINTF(a) fprintf a
+# endif //unicode
+#else //win32
+#	define FPRINTF(a) fprintf a
+#endif //win32
+
+#define cutilSafeCall(err)           __cudaSafeCall      (err, __FILE__, __LINE__)
+
+inline void __cudaSafeCall( cudaError err, const char *file, const int line )
+{
+    if( cudaSuccess != err) {
+		FPRINTF((stderr, "%s(%i) : cudaSafeCall() Runtime API error : %s.\n",
+                file, line, cudaGetErrorString( err) ));
+        exit(-1);
+    }
+}
+
+#define MIN(a,b) ((a < b) ? a : b)
+#define MAX(a,b) ((a > b) ? a : b)
+
+// Beginning of GPU Architecture definitions
+inline int _ConvertSMVer2Cores(int major, int minor)
+{
+	// Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
+	typedef struct {
+		int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
+		int Cores;
+	} sSMtoCores;
+
+	sSMtoCores nGpuArchCoresPerSM[] = 
+	{ { 0x10,  8 },
+	  { 0x11,  8 },
+	  { 0x12,  8 },
+	  { 0x13,  8 },
+	  { 0x20, 32 },
+	  { 0x21, 48 },
+	  {   -1, -1 } 
+	};
+
+	int index = 0;
+	while (nGpuArchCoresPerSM[index].SM != -1) {
+		if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor) ) {
+			return nGpuArchCoresPerSM[index].Cores;
+		}
+		index++;
+	}
+	printf("MapSMtoCores undefined SMversion %d.%d!\n", major, minor);
+	return -1;
+}
+// end of GPU Architecture definitions
+
+// This function returns the best GPU (with maximum GFLOPS)
+inline int cutGetMaxGflopsDeviceId()
+{
+	int current_device   = 0, sm_per_multiproc = 0;
+	int max_compute_perf = 0, max_perf_device  = 0;
+	int device_count     = 0, best_SM_arch     = 0;
+	cudaDeviceProp deviceProp;
+
+	cudaGetDeviceCount( &device_count );
+	// Find the best major SM Architecture GPU device
+	while ( current_device < device_count ) {
+		cudaGetDeviceProperties( &deviceProp, current_device );
+		if (deviceProp.major > 0 && deviceProp.major < 9999) {
+			best_SM_arch = MAX(best_SM_arch, deviceProp.major);
+		}
+		current_device++;
+	}
+
+    // Find the best CUDA capable GPU device
+	current_device = 0;
+	while( current_device < device_count ) {
+		cudaGetDeviceProperties( &deviceProp, current_device );
+		if (deviceProp.major == 9999 && deviceProp.minor == 9999) {
+		    sm_per_multiproc = 1;
+		} else {
+			sm_per_multiproc = _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor);
+		}
+
+		int compute_perf  = deviceProp.multiProcessorCount * sm_per_multiproc * deviceProp.clockRate;
+		if( compute_perf  > max_compute_perf ) {
+            // If we find GPU with SM major > 2, search only these
+			if ( best_SM_arch > 2 ) {
+				// If our device==dest_SM_arch, choose this, or else pass
+				if (deviceProp.major == best_SM_arch) {	
+					max_compute_perf  = compute_perf;
+					max_perf_device   = current_device;
+				}
+			} else {
+				max_compute_perf  = compute_perf;
+				max_perf_device   = current_device;
+			}
+		}
+		++current_device;
+	}
+	return max_perf_device;
+}
